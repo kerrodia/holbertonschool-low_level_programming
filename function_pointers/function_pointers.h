bcb9d0813c694cdb210fd0f0d5269ea49d64e422
kerrodia <5658@holbertonstudents.com>
2022-11-03 06:20:22 -0700

    Write a function that prints a name.

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,14 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
-/**
- * File: function_pointers.h
- * AUth: Kollo Ricardo
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
-
-#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
