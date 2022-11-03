6ad5870e559826e73205bc344b151bd3eabe897c
kerrodia <5658@holbertonstudents.com>
2022-11-03 03:42:03 -0700

    Write a function that prints a name.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,9 +3,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name from a function pointer.
+ * print_name - prints a name from a function pointer.
  * @name: name char string
- * @f: A pointer to a function that prints a name.
+ * @f: a pointer to a function that prints a name.
  * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
